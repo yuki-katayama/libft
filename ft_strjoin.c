2fd2f3db11e8bdc4681d29f2ab7c7c6bcf599cc4
yuki-katayama <katayu810@gmail.com>
2021-04-09 09:04:37 +0900

    update

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: kyuki <kyuki@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/11 03:10:17 by kyuki             #+#    #+#             */
-/*   Updated: 2021/04/05 12:22:05 by kyuki            ###   ########.fr       */
+/*   Updated: 2021/04/05 18:11:35 by kyuki            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		return (NULL);
 	length = ft_strlen(s1);
 	length += ft_strlen(s2);
-	if (!(ft_malloc_p((void **)&dest, sizeof(char) * length)))
+	if (!(ft_malloc_p((void **)&dest, sizeof(char) * length + 1)))
 		return (NULL);
 	while (s1[i] != '\0')
 	{
