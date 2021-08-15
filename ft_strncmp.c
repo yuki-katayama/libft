bfdc8b0ab79acc40ae29d5fd477820a634f088da
yuki-katayama <katayu810@gmail.com>
2021-08-15 16:50:42 +0900

    fix: ft_strncmp普通にミスってた〜〜

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kyuki <kyuki@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/09/27 01:26:35 by kyuki             #+#    #+#             */
-/*   Updated: 2021/08/15 15:34:53 by kyuki            ###   ########.fr       */
+/*   Created: 2021/08/15 16:48:59 by kyuki             #+#    #+#             */
+/*   Updated: 2021/08/15 16:49:04 by kyuki            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,12 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned int	i;
+	int	i;
 
-	i = -1;
+	i = 0;
 	if (n == 0)
 		return (0);
-	while (++i < n && s1[i] && s2[i] && s1[i] == s2[i])
-		;
-	i -= 1;
-	return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+	while (i < (int)n - 1 && s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (((char *)s1)[i] - ((char *)s2)[i]);
 }
