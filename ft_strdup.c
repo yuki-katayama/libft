11b89aa3be07c8295b246ccdfaf787d6590d61c6
yuki-katayama <katayu810@gmail.com>
2021-08-29 12:19:26 +0900

    refactor: ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,29 +6,22 @@
 /*   By: kyuki <kyuki@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/11 03:10:10 by kyuki             #+#    #+#             */
-/*   Updated: 2021/04/05 12:22:05 by kyuki            ###   ########.fr       */
+/*   Updated: 2021/08/29 12:19:11 by kyuki            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(char *src)
+char	*ft_strdup(const char *src)
 {
 	char	*dest;
-	int		size;
 	int		i;
 
-	size = 0;
-	while (src[size])
-		size++;
-	if (!(ft_malloc_p((void **)&dest, sizeof(char) * size + 1)))
+	if (!(ft_malloc_p((void **)&dest, sizeof(char *) * (ft_strlen(src) + 1))))
 		return (NULL);
 	i = 0;
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
+	while (*src)
+		dest[i++] = *src++;
 	dest[i] = '\0';
 	return (dest);
 }
--- libft.h
+++ libft.h
@@ -23,7 +23,7 @@ int				ft_strncmp(const char *s1, const char *s2, size_t n);
 int				ft_strcmp(const char *s1, const char *s2);
 char			*ft_strncat(char *s1, const char *s2, size_t n);
 size_t			ft_strlen(const char *s);
-char			*ft_strdup(char *src);
+char			*ft_strdup(const char *src);
 char			*ft_strcpy(char *dst, const char *src);
 char			*ft_strcat(char *s1, const char *s2);
 int				ft_isprint(int c);
