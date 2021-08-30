bde26b347de8bf89fb536caa286273ec36461a6e
yuki-katayama <katayu810@gmail.com>
2021-08-31 08:12:04 +0900

    refactor: ft_splitを再起を使用したものに変更

--- ft_split.c
+++ ft_split.c
@@ -5,90 +5,61 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kyuki <kyuki@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/11 03:09:46 by kyuki             #+#    #+#             */
-/*   Updated: 2021/06/05 18:32:05 by kyuki            ###   ########.fr       */
+/*   Created: 2021/08/31 08:10:39 by kyuki             #+#    #+#             */
+/*   Updated: 2021/08/31 08:11:07 by kyuki            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	check_if_sep(char c, char charset)
+static char	*sub_split(const char *start, const char *end)
 {
-	if (c == charset)
-		return (1);
-	return (0);
-}
+	char	*ret;
+	size_t	idx;
 
-static int	cnt_str(char const *str, char sep)
-{
-	int	i;
-	int	count;
-
-	i = 0;
-	count = 0;
-	while (str[i])
-	{
-		while (check_if_sep(str[i], sep))
-			i++;
-		count++;
-		while (str[i] && !(check_if_sep(str[i], sep)))
-			i++;
-	}
-	return (count);
-}
-
-static int	ft_strlen_sep(char const *s, char sep)
-{
-	int	i;
-
-	i = 0;
-	while (s[i] && (!check_if_sep(s[i], sep)))
-		i++;
-	return (i);
-}
-
-static char	*assign_res(char const *str, char sep)
-{
-	int		i;
-	char	*res;
-
-	if (!(ft_malloc_p((void **)&res, (ft_strlen_sep(str, sep) + 1))))
+	if (!(ft_malloc_p((void **)&ret, sizeof(char *) * (end - start + 1))))
 		return (NULL);
-	i = 0;
-	while (str[i] && !(check_if_sep(str[i], sep)))
-	{
-		res[i] = str[i];
-		i++;
-	}
-	res[i] = '\0';
-	return (res);
+	if (!ret)
+		return (NULL);
+	idx = 0;
+	while (start != end)
+		ret[idx++] = *start++;
+	ret[idx] = '\0';
+	return (ret);
 }
 
-char	**ft_split(char const *s, char c)
+static char	**rec_split(const char *str, const char c, size_t idx)
 {
-	char	**words;
-	size_t	i;
-	int		j;
+	char	**ret;
+	char	*ptr;
 
+	while (*str && *str == c)
+		++str;
+	if (!*str)
+	{
+		if (!(ft_malloc_p((void **)&ret, sizeof(char *) * (idx + 1))))
+			return (NULL);
+		if (ret)
+			ret[idx] = NULL;
+		return (ret);
+	}
+	ptr = (char *)str;
+	while (*str && *str != c)
+		++str;
+	ptr = sub_split(ptr, str);
+	if (!ptr)
+		return (NULL);
+	ret = rec_split(str, c, idx + 1);
+	if (ret)
+		ret[idx] = ptr;
+	else
+		free(ptr);
+	return (ret);
+}
+
+char	**ft_split(const char *s, const char c)
+{
 	if (!s)
 		return (NULL);
-	if (!(ft_malloc_pp((void ***)&words, sizeof(char *) * (cnt_str(s, c) + 1))))
-		return (NULL);
-	j = 0;
-	i = 0;
-	while (s[i] && s != NULL)
-	{
-		while (s[i] && check_if_sep(s[i], c))
-			i++;
-		if (s[i])
-		{
-			words[j] = assign_res(s + i, c);
-			if (!(words[j++]))
-				return (ft_free_char(words, (int)i));
-		}
-		while (s[i] && !(check_if_sep(s[i], c)))
-			i++;
-	}
-	words[j] = NULL;
-	return (words);
+	return (rec_split((char *)s, c, 0));
 }
