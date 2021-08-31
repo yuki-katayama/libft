09e82a0a32871be0cebaf9d705231d6222f1adee
yuki-katayama <katayu810@gmail.com>
2021-08-31 20:31:30 +0900

    update: chardel

--- ft_chardel.c
+++ ft_chardel.c
@@ -1,23 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_chardel.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kyuki <kyuki@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/31 20:31:19 by kyuki             #+#    #+#             */
+/*   Updated: 2021/08/31 20:31:20 by kyuki            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-static int	ft_getlen(char *str, char c)
+static bool	is_delete(char str, char *c)
+{
+	if (ft_strchr(c, str))
+		return (true);
+	return (false);
+}
+
+static int	ft_getlen(char *str, char *c)
 {
 	int	len1;
 	int	len2;
 	int	i;
 
-	i = 0;
+	i = -1;
 	len1 = ft_strlen(str);
 	len2 = len1;
 	while (++i < len1)
 	{
-		if (str[i] == c)
+		if (is_delete(str[i], c))
 			len2--;
 	}
 	return (len2);
 }
 
-char	*ft_chardel(char *str, char c)
+char	*ft_chardel(char *str, char *c)
 {
 	char	*ret;
 	int		len;
@@ -32,7 +51,7 @@ char	*ft_chardel(char *str, char c)
 	len = ft_strlen(str);
 	while (++i < len)
 	{
-		if (str[i] != c)
+		if (!(is_delete(str[i], c)))
 			ret[++j] = str[i];
 	}
 	ret[i] = '\0';
